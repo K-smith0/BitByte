ba4bf92a4518ea28eab15222e0be7e0f0d043424
K-Smith0 <122173ks@gmail.com>
2025-02-03 10:36:31 +0100

    progress

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let inputs = {w:false,a:false,s:false,d:false};
 ctx.strokeStyle="green";
 ctx.lineWidth=7;
 class player{
+    h=30;
+    w=15;
     constructor(pos){
         this.position={x:pos.x,y:pos.y,dx:0,dy:0,ddx:0,ddy:0}
         this.grounded=true;
@@ -12,24 +14,15 @@ class player{
     debugDraw(){
         ctx.translate(500-this.position.x,500-this.position.y);
         ctx.beginPath();
-        ctx.rect(this.position.x,this.position.y,15,-30);
-        ctx.rect(this.position.x,this.position.y,-15,-30);
-        ctx.rect(this.position.x,this.position.y,-15,30);
-        ctx.rect(this.position.x,this.position.y,15,30);
+        ctx.rect(this.position.x,this.position.y,this.w,-this.h);
+        ctx.rect(this.position.x,this.position.y,-this.w,-this.h);
+        ctx.rect(this.position.x,this.position.y,-this.w,this.h);
+        ctx.rect(this.position.x,this.position.y,this.w,this.h);
         ctx.rect(this.position.x-5,this.position.y-5,10,10);
-        ctx.rect(500,500,20,20);
         ctx.stroke();
         ctx.closePath();
         ctx.resetTransform();
     }
-    /*  
-    this.position.ddy=(this.position.ddy+10>10)?10:(this.position.ddy<10)?this.position.ddy+10:0;
-    this.position.dx=this.position.dx*0.9+this.position.ddx;
-    this.position.dy=this.position.dy*0.9+this.position.ddy;
-    this.position.x=(this.position.x+this.position.dx<0)?0:(this.position.x+this.position.dx>970)?970:this.position.x+this.position.dx;
-    this.position.y=(this.position.y+this.position.dy>1000)?1000:this.position.y+this.position.dy;
-    this.debugDraw(); 
-    */
     debugUpdateInput(){
         if(inputs.w && this.grounded){
             this.position.ddy-=30;
@@ -38,17 +31,18 @@ class player{
         if(inputs.a)this.position.ddx=(this.position.ddx=-1)?-2:this.position.ddx-1*deltaTime;
         if(inputs.d)this.position.ddx=(this.position.ddx=1)?2:this.position.ddx+1*deltaTime;
         this.position.ddy=(this.position.ddy+100>100)?100*deltaTime:(this.position.ddy<100)?this.position.ddy+100*deltaTime:0;
-
+        if(inputs.s){this.h=15; this.position.y+15;}
+        else {this.h=30;this.position-15;}
 
     }
     debugUpdatePosition(){
         this.position.dx=this.position.dx*0.9+this.position.ddx;
         this.position.dy=this.position.dy*0.9+this.position.ddy;
-        this.position.x=(this.position.x+this.position.dx<15)?15:(this.position.x+this.position.dx>985)?985:this.position.x+this.position.dx;
-        this.position.y=(this.position.y+this.position.dy>970)?970:this.position.y+this.position.dy;
-        this.position.dy=(this.position.y==970)?0:this.position.dy;
-        this.position.dx=(this.position.x==15 || this.position.x==985)?0:this.position.dx;
-        if(this.position.y==970)this.grounded=true;
+        this.position.x=this.position.x+this.position.dx;
+        //this.position.y=(this.position.y+this.position.dy>1000-this.h)?1000-this.h:this.position.y+this.position.dy;
+        this.position.dy=(this.position.y==1000-this.h)?0:this.position.dy;
+        this.position.dx=(this.position.x==this.w || this.position.x==1000-this.w)?0:this.position.dx;
+        //if(this.position.y==1000-this.h)this.grounded=true;
         this.position.ddy=0;
         this.position.ddx=0;
     }
@@ -62,7 +56,31 @@ class player{
     }
 
 }
+class map{
+    map=[];
+    constructor(){
+        for (let i = 1; i <= 100; i++) for (let j = 1; j <= 100; j++) this.map.push({x:i,y:j,solid:(Math.random()>0.7)});
+    }
+    debugDraw(){
+        ctx.translate(500-p.position.x,500-p.position.y);
+        this.map.filter(x => ((p.position.x/50)-10<x.x && (p.position.x/50)+10>x.x && (p.position.y/50)-10<x.y && (p.position.y/50)+10>x.y )).forEach(x => {
+            if(x.solid){
+            ctx.beginPath();
+            ctx.strokeStyle="blue";
+            ctx.rect(x.x*50,x.y*50,25,-25);
+            ctx.rect(x.x*50,x.y*50,-25,-25);
+            ctx.rect(x.x*50,x.y*50,-25,25);
+            ctx.rect(x.x*50,x.y*50,25,25);
+            ctx.stroke();
+            ctx.strokeStyle="green";
+            ctx.closePath();
+        }
+        });
+        ctx.resetTransform();
+    }
+}
 let p = new player({x:500,y:500});
+let Mapper = new map();
 
 document.onkeydown= (e) => inputs[`${e.key.toLowerCase()}`]=true;
 
@@ -72,4 +90,5 @@ document.onkeyup= (e) => inputs[`${e.key.toLowerCase()}`]=false;
 setInterval(x => {
     ctx.clearRect(0,0,1000,1000);
     p.debugUpdate();
+    Mapper.debugDraw();
 },16);
